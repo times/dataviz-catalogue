f01d3f347d78e8fae3ae6d520a13705aaec7361c
basilesimon <basile@basilesimon.fr>
2018-05-18 15:49:58 +0100

    WIP small multiple implementation

--- small-multiples/script.js
+++ small-multiples/script.js
@@ -13,22 +13,60 @@ labels.map(function(symbol) {
   }
 });
 
-const parseDate = d3.timeParse('%Y');
+const width = document
+  .getElementsByClassName('container')[0]
+  .getBoundingClientRect().width;
+const config = {
+  parseDate: d3.timeParse('%Y'),
+  chartWidth: width < 450 ? width * 0.7 : width / 3,
+  chartHeight: 100,
+  chartMargin: { top: 20, right: 0, bottom: 10, left: 20 },
+  area: d3.area().curve(d3.curveStep),
+  line: d3.line().curve(d3.curveStep),
+  xScale: d3.scaleTime(),
+  yScale: d3.scaleLinear(),
+};
 
-const chart = timeSeriesChart()
-  .x(d => parseDate(d.date))
-  .y(d => +d.price)
-  .width(200)
-  .height(100)
-  .values(d => d.values)
-  .title(d => d.key)
-  .titleX(25)
-  .titleY(-10)
-  .margin({ top: 20, right: 20, bottom: 20, left: 10 });
+const usableWidth =
+  config.chartWidth - config.chartMargin.left - config.chartMargin.right;
+const usableHeight =
+  config.chartHeight - config.chartMargin.top - config.chartMargin.bottom;
 
-chart.xExtent(d3.extent(dataset, chart.x()));
+const symbols = d3
+  .nest()
+  .key(d => d.symbol)
+  .entries(dataset);
 
-const symbols = d3.nest().key(d => d.symbol).entries(dataset);
+// this function appends an SVG and a mini chart to a div
+function makeSmallChart(d, i) {
+  const { values } = d;
+  const xScale = config.xScale
+    .domain(d3.extent(values, d => d.date))
+    .range([0, usableWidth]);
+
+  const yScale = config.yScale
+    .domain(d3.extent(values, d => d.price))
+    .range([usableHeight, 0]);
+
+  const line = config.line.x(d => xScale(d.date)).y(d => yScale(d.price));
+  const area = config.area
+    .x(d => xScale(d.date))
+    .y1(d => yScale(d.price))
+    .y0(yScale(0));
+
+  const chart = d3
+    .select(this)
+    .append('svg')
+    .at({ width: config.chartWidth, height: config.chartHeight })
+    .append('g')
+    .translate([config.chartMargin.left, config.chartMargin.top]);
+
+  chart.append('path').at({ d: area(d.values), class: 'area' });
+  chart.append('path').at({
+    d: line(d.values),
+    class: 'line',
+  });
+}
 
 d3
   .select('.container')
@@ -37,4 +75,4 @@ d3
   .enter()
   .append('div')
   .attr('class', 'chart')
-  .call(chart);
+  .each(makeSmallChart);
